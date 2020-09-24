bd8579803d9998f73bafca57ee3175200ed10d63
atom-young <atom.yang@aelf.io>
2020-09-24 20:55:44 +0800

    remove wallet key and google id temporily

--- src/index.tsx
+++ src/index.tsx
@@ -31,7 +31,7 @@ if (typeof GOOGLE_ANALYTICS_ID === 'string') {
     customBrowserType: !isMobile ? 'desktop' : 'web3' in window || 'ethereum' in window ? 'mobileWeb3' : 'mobileRegular'
   })
 } else {
-  ReactGA.initialize('test', { testMode: true, debug: true })
+  // ReactGA.initialize('test', { testMode: true, debug: true })
 }
 
 window.addEventListener('error', error => {
